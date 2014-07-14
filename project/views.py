8dc0ec91f0d1fc21e1ae40f3cf33370188feada6
chingalo <profschingalo@fmail.com>
2014-07-14 10:03:43 +0300

    adding functions for login process

--- project/views.py
+++ project/views.py
@@ -11,3 +11,29 @@ def index(request):
 	context = {}
 	return render(request,'index.html',context)
 	
+#processing login processs
+def login(request):
+	
+	userFromSystem = Users.objects.all()
+	word = ''	
+	
+	if request.POST:
+		#taking values from form		
+		form = request.POST		
+		emailListPosted = form.getlist('email')
+		passwordPosted = form.getlist('password')
+		
+		details = emailListPosted[0] + " " + passwordPosted[0]
+		
+		#check if login user is registered
+		for user in userFromSystem:
+			if user.e_mail == emailListPosted[0] and user.password == passwordPosted[0]:				
+				word = 'successfully login in the system'
+				
+			else :
+				word = 'not login in the system please login in or register'
+		
+	context = {'m':'hello statrting login process',
+				'form':word,'details':details,
+			}
+	return render(request,'userFunction.html',context)
