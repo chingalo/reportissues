b11845ee18effd0790e8820c4a5569a0e5908c93
chingalo <profschingalo@fmail.com>
2014-07-15 15:22:38 +0300

    update custom script for validation of emails

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,25 +1,41 @@
 
-
 //validation of form on submit if all required fields have been filled
 function validateForm(){
 	for(var i = 0;i < myform.elements.length;i++){	
 		if(myform.elements[i].className == "required" && myform.elements[i].value.length == 0){			
-			alert('Fail to create new account, please make sure you fill required fields '+email);
+			alert('Fail to create new account, please make sure you fill required fields ');
 			return false;			
 			}		
 		}
-	
-		
-	
-	}
-
-//e-mail validation on the form
-function chechEmail(){
 	var email = document.getElementById('email').value;
 	var atpos = email.indexOf('@');
 	var atdot = email.lastIndexOf('.');
-	
+
 	if(atpos < 1 || atdot < atpos+2 || atdot+2 >= email.length){
 		alert('incorrect email');
 		}	
 	}
+
+
+	
+
+//e-mail validation on the form
+function chechEmailLogin(){
+	var email = document.getElementById('emaillogin').value;
+	var atpos = email.indexOf('@');
+	var atdot = email.lastIndexOf('.');
+
+	if(atpos < 1 || atdot < atpos+2 || atdot+2 >= email.length){
+		alert('incorrect email');
+		}	
+	}
+	
+function chechEmail(){
+	var email = document.getElementById('email').value;
+	var atpos = email.indexOf('@');
+	var atdot = email.lastIndexOf('.');
+
+	if(atpos < 1 || atdot < atpos+2 || atdot+2 >= email.length){
+		alert('incorrect email');
+		}	
+	}	
