770792b53c43e754fa81e0c3693d04cf78f201aa
chingalo <profschingalo@fmail.com>
2014-07-17 10:41:01 +0300

    update script

--- static/js/validation.js
+++ static/js/validation.js
@@ -21,7 +21,7 @@ function validateForm(){
 function editProfile(){
 	for(var i = 0;i < myform.elements.length;i++){	
 		if(myform.elements[i].className == "required" && myform.elements[i].value.length == 0){			
-			alert('Fail to create new account, please make sure you fill required fields ');
+			alert('Fail to edit your account, please make sure you fill required fields ');
 			return false;			
 			}		
 		}
