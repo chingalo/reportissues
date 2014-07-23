b41cd55e2e484444219268801db73712f8166553
chingalo <profschingalo@fmail.com>
2014-07-23 14:15:35 +0300

    initiate animations

---
+++ static/js/customeffect.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+   //animation for all neccessary info for system
+   $(".event1").hide(2000).delay(100).slideDown(2000);
+   $(".event2").hide(800).show(2000);
+   $(".event3").hide(1000).show(1600);
+   $(".event4").hide(1500).show(3000);
+});
