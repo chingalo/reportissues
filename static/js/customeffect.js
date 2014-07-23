2694912daa654b6bc2d8ed45c983388c9781eb19
chingalo <profschingalo@fmail.com>
2014-07-23 15:35:22 +0300

    update to second round animation

--- static/js/customeffect.js
+++ static/js/customeffect.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
    //animation for all neccessary info for system
-   //delay() slideUp() slideDown() fadeOut() fadeIn() show() hide()
+   //delay() slideUp() fadeOut() show() hide()
    //event 1
-   $(".event1").hide().show(2000).delay(1000).hide(2000);
+   $(".event1").hide().show(2000).delay(2000).hide(2000).delay(12000).show(2000).delay(2000).hide(2000);
    
    
    //event 2
-   $(".event2").hide().delay(3000).show(2000).delay(1000).slideUp(2000);
+   $(".event2").hide().delay(4000).show(2000).delay(2000).slideUp(2000).delay(12000).show(2000).delay(2000).hide(2000);
    
    
-   //event 3
-   $(".event3").hide().delay(6000).show(2000).delay(1000).hide(2000);
+   //event 3.
+   $(".event3").hide().delay(8000).show(2000).delay(2000).hide(2000).delay(12000).show(2000).delay(2000).slideUp(2000);
    
    
    //event 4
-   $(".event4").hide().delay(9000).fadeIn(2000).delay(1000);
+   $(".event4").hide().delay(12000).fadeIn(2000).delay(2000).slideUp(2000).delay(12000).show(2000).delay(2000).hide(2000);
    
    
 });
